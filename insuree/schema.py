d89acf137e88616bbfa1e3ef204958ccd619c3fe
xgill <38523490+xgill@users.noreply.github.com>
2019-07-29 16:24:28 +0200

    Feature/enquiry (#2)
    * removed rowids
    
    * intermediate commit
    
    * intermediate 2
    
    * bumped version 0.0.3

--- insuree/schema.py
+++ insuree/schema.py
@@ -1,16 +1,66 @@
+import re
+from django.db.models import Q
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Insuree
+from .models import Insuree, Photo, Gender, Family, FamilyType
+import core
 
 
-class InsureeType(DjangoObjectType):
+class GenderGraphQLType(DjangoObjectType):
+    class Meta:
+        model = Gender
+
+
+class PhotoGraphQLType(DjangoObjectType):
+    class Meta:
+        model = Photo
+
+
+class FamilyTypeGraphQLType(DjangoObjectType):
+    class Meta:
+        model = FamilyType
+
+
+class FamilyGraphQLType(DjangoObjectType):
+    class Meta:
+        model = Family
+
+
+class InsureeGraphQLType(DjangoObjectType):
+    age = graphene.Int(source='age')
+
     class Meta:
         model = Insuree
-        exclude_fields = ('row_id',)
 
 
 class Query(graphene.ObjectType):
-    all_insurees = graphene.List(InsureeType)
+    insuree = graphene.Field(
+        InsureeGraphQLType,
+        chfId=graphene.String(required=True),
+        validity=graphene.String()
+    )
+    insuree_family_members = graphene.List(
+        InsureeGraphQLType,
+        chfId=graphene.String(required=True),
+        validity=graphene.String()
+    )
 
-    def resolve_all_insurees(self, info, **kwargs):
-        return Insuree.objects.all()
+    @staticmethod
+    def _resolve_insuree(info, **kwargs):
+        try:
+            return Insuree.objects.get(
+                Q(chf_id=kwargs.get('chfId')),
+                *core.utils.filter_validity(**kwargs)
+            )
+        except Insuree.DoesNotExist:
+            return None
+
+    def resolve_insuree(self, info, **kwargs):
+        return Query._resolve_insuree(info=info, **kwargs)
+
+    def resolve_insuree_family_members(self, info, **kwargs):
+        insuree = Query._resolve_insuree(info=info, **kwargs)
+        return Insuree.objects.filter(
+            Q(family=insuree.family),
+            *core.utils.filter_validity(**kwargs)
+        ).order_by('-head')
