2454836b68f1fdc6c13ebb7cb96e28600b09affa
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-08-11 20:59:39 +0200

    validity filters as Date

--- insuree/schema.py
+++ insuree/schema.py
@@ -5,7 +5,7 @@ from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import Insuree, Photo, Gender, Family, FamilyType
-import core
+from core import filter_validity
 
 
 class GenderGQLType(DjangoObjectType):
@@ -34,9 +34,10 @@ class InsureeGQLType(DjangoObjectType):
     class Meta:
         model = Insuree
         filter_fields = {
-            "id": ["exact"],
+            "chf_id": ["exact"],
             "last_name": ["exact", "istartswith", "icontains", "iexact"],
             "other_names": ["exact", "istartswith", "icontains", "iexact"],
+            "gender": ["exact"]
         }
         interfaces = (graphene.relay.Node,)
 
@@ -45,30 +46,30 @@ class Query(graphene.ObjectType):
     insuree = graphene.relay.node.Field(
         InsureeGQLType,
         chfId=graphene.String(required=True),
-        validity=graphene.String()
+        validity=graphene.Date()
     )
-    insuree_family_members = DjangoFilterConnectionField(
+    insuree_family_members = graphene.List(
         InsureeGQLType,
         chfId=graphene.String(required=True),
-        validity=graphene.String()
+        validity=graphene.Date()
     )
 
     @staticmethod
     def _resolve_insuree(info, **kwargs):
-        try:
-            return Insuree.objects.get(
-                Q(chf_id=kwargs.get('chfId')),
-                *core.utils.filter_validity(**kwargs)
-            )
-        except Insuree.DoesNotExist:
-            return None
+        return Insuree.objects.get(
+            Q(chf_id=kwargs.get('chfId')),
+            *filter_validity(**kwargs)
+        )
 
     def resolve_insuree(self, info, **kwargs):
-        return Query._resolve_insuree(info=info, **kwargs)
+        try:
+            return Query._resolve_insuree(info=info, **kwargs)
+        except Insuree.DoesNotExist:
+            return None
 
     def resolve_insuree_family_members(self, info, **kwargs):
         insuree = Query._resolve_insuree(info=info, **kwargs)
         return Insuree.objects.filter(
             Q(family=insuree.family),
-            *core.utils.filter_validity(**kwargs)
+            *filter_validity(**kwargs)
         ).order_by('-head')
