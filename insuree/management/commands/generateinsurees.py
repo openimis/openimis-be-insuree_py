05c2e862cdfdc1d66d09e023285ae7da225f70d8
Eric Darchis <darchis@pobox.com>
2021-02-22 06:54:27 +0100

    Add optional policy creation
    Creating an insuree without an active policy is not so useful so we added the option to create a policy for each insuree. The policy and product modules are only loaded if this option is specified so one can still create insurees without the policy and product modules.

--- insuree/management/commands/generateinsurees.py
+++ insuree/management/commands/generateinsurees.py
@@ -11,10 +11,17 @@ class Command(BaseCommand):
     insurees = None
     services = None
     items = None
+    products = None
 
     def add_arguments(self, parser):
         parser.add_argument("nb_insurees", nargs=1, type=int)
         parser.add_argument("nb_members", nargs=1, type=int)
+        parser.add_argument(
+            '--policy',
+            action='store_true',
+            dest='policy',
+            help='Create a policy for each family, it will be active',
+        )
         parser.add_argument(
             '--verbose',
             action='store_true',
@@ -24,7 +31,7 @@ class Command(BaseCommand):
         parser.add_argument(
             '--locale',
             default="fr_FR",
-            help="Don't use the Faker library but rather dummy numbered strings",
+            help="Used to adapt the fake names generation to the locale, using Faker, by default fr_FR",
         )
 
     def handle(self, *args, **options):
@@ -32,6 +39,7 @@ class Command(BaseCommand):
         nb_insurees = options["nb_insurees"][0]
         nb_members = options["nb_members"][0]
         verbose = options["verbose"]
+        policy = options["policy"]
         for insuree_num in range(1, nb_insurees + 1):
             props = dict(
                 last_name=fake.last_name(),
@@ -51,3 +59,16 @@ class Command(BaseCommand):
                 member = create_test_insuree(with_family=False, custom_props=props)
                 if verbose:
                     print("Created family member", member_num, member.other_names)
+
+            if policy:
+                from policy.test_helpers import create_test_policy_family
+                product = self.get_random_product()
+                policy = create_test_policy_family(product, insuree.family, link=True, valid=True)
+                if verbose:
+                    print("Generated policy for family", insuree.family_id, policy)
+
+    def get_random_product(self):
+        if not self.products:
+            from product.models import Product
+            self.products = Product.objects.filter(validity_to__isnull=True).values_list("pk", flat=True)
+        return random.choice(self.products)
