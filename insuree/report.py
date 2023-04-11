af5d4ff05ebcf3c009db071c51e50e545b2089ce
Eric Darchis <darchis@pobox.com>
2023-04-11 07:58:55 +0200

    Add missing insuree photos report
    Making this query in Django ORM is a overly complex because of the union between ClaimItem and ClaimService that are used for joining to Claim and Product
    
    ```mermaid
    flowchart TD
        subgraph CDetails
            ClaimItem
            ClaimService
        end
        CDetails --> Claim
        CDetails --> Product
        Insuree --> Family
        Family --> Village
        Village --> Ward
        Ward --> District
        District --> Region
        Claim --> HF
        Claim --> Insuree
        Claim --> ClaimAdmin
    ```

--- insuree/report.py
+++ insuree/report.py
@@ -1,9 +1,19 @@
-from insuree.reports import insuree_family_overview, enrolled_families
+from insuree.reports import insuree_family_overview, enrolled_families, insuree_missing_photo
 from insuree.reports.enrolled_families import enrolled_families_query
 from insuree.reports.insuree_family_overview import insuree_family_overview_query
+from insuree.reports.insuree_missing_photo import insuree_missing_photo_query
 
 # Insuree_family_overview are the same report, with native code and with the stored_procedure
 report_definitions = [
+    {
+        "name": "insuree_missing_photo",
+        "engine": 0,
+        "default_report": insuree_missing_photo.template,
+        "description": "Missing insuree photos",
+        "module": "insuree",
+        "python_query": insuree_missing_photo_query,
+        "permission": ["131215"],
+    },
     {
         "name": "insuree_family_overview",
         "engine": 0,
