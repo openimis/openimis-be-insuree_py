8ee94a1aec55d76765e04ff2628411b33b4fc15d
Eric Darchis <darchis@pobox.com>
2023-04-14 08:47:29 +0200

    Feature/omt 320 pending enrollments (#57)
    * Add missing insuree photos report
    
    Making this query in Django ORM is a overly complex because of the union between ClaimItem and ClaimService that are used for joining to Claim and Product
    
    ```mermaid
    flowchart TD
        subgraph CDetails
            ClaimItem
            ClaimService
        end
        CDetails --> Claim
        CDetails --> Product
        Insuree --> Family
        Family --> Village
        Village --> Ward
        Ward --> District
        District --> Region
        Claim --> HF
        Claim --> Insuree
        Claim --> ClaimAdmin
    ```
    
    * OMT-320 pending enrollments report

--- insuree/report.py
+++ insuree/report.py
@@ -1,7 +1,10 @@
-from insuree.reports import insuree_family_overview, enrolled_families, insuree_missing_photo
+from insuree.reports import insuree_family_overview, enrolled_families, insuree_missing_photo, \
+    insurees_pending_enrollment
 from insuree.reports.enrolled_families import enrolled_families_query
 from insuree.reports.insuree_family_overview import insuree_family_overview_query
 from insuree.reports.insuree_missing_photo import insuree_missing_photo_query
+from insuree.reports.insurees_pending_enrollment import insurees_pending_enrollment_query
+
 
 # Insuree_family_overview are the same report, with native code and with the stored_procedure
 report_definitions = [
@@ -12,6 +15,15 @@ report_definitions = [
         "description": "Missing insuree photos",
         "module": "insuree",
         "python_query": insuree_missing_photo_query,
+        "permission": ["131215"],
+    },
+    {
+        "name": "insurees_pending_enrollment",
+        "engine": 0,
+        "default_report": insurees_pending_enrollment.template,
+        "description": "Insurees pending enrollment",
+        "module": "insuree",
+        "python_query": insurees_pending_enrollment_query,
         "permission": ["131215"],
     },
     {
