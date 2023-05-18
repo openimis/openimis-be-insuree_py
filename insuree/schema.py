c9f88d428efab6ed9d3635921f09e98dcc282850
lruzicki <lruzicki@gmail.com>
2023-05-18 15:54:30 +0200

    OTC-1016: claim insuree perms added

--- insuree/schema.py
+++ insuree/schema.py
@@ -149,7 +149,8 @@ class Query(graphene.ObjectType):
         return Gender.objects.order_by('sort_order').all()
 
     def resolve_insurees(self, info, **kwargs):
-        if not info.context.user.has_perms(InsureeConfig.gql_query_insurees_perms):
+        if not info.context.user.has_perms(InsureeConfig.gql_query_insurees_perms) \
+                and not info.context.user.has_perms(ClaimConfig.gql_query_claims_perms):
             raise PermissionDenied(_("unauthorized"))
         filters = []
         additional_filter = kwargs.get('additional_filters', None)
@@ -191,6 +192,17 @@ class Query(graphene.ObjectType):
                 uuid__in=user_districts.values_list('location__uuid', flat=True)))]
 
         return gql_optimizer.query(Insuree.objects.filter(*filters).all(), info)
+
+    def resolve_insuree_by_chf_id(self, info, chf_id):
+        if not info.context.user.has_perms(InsureeConfig.gql_query_insuree_perms):
+            raise PermissionDenied(_("unauthorized"))
+
+        try:
+            insuree = Insuree.objects.get(chf_id=chf_id, validity_to__isnull=True)
+        except Insuree.DoesNotExist:
+            return None
+
+        return insuree
 
     def resolve_family_members(self, info, **kwargs):
         if not info.context.user.has_perms(InsureeConfig.gql_query_insuree_family_members):
