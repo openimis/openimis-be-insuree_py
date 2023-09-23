8e8d8a8a0332f19d2cac7543fe7221e7f171ada9
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2023-09-23 11:23:00 +0200

    Update test_insuree_photo.py

--- insuree/tests/test_insuree_photo.py
+++ insuree/tests/test_insuree_photo.py
@@ -30,19 +30,22 @@ class InsureePhotoTest(TestCase):
         "language": "en",
         "roles": [4],
     }
-    photo_base64 = "iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAOxAAADsQBlSsOGwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAB7SURBVAiZLc0xDsIwEETRP+t1wpGoaDg6DUgpEAUNNyH2DkXon/S03W+uKiIaANmS07Jim2ytc75cAWMbAJF8Xg9iycQV1AywALCh9yTWtXN4Yx9Agu++EyAkA0IxQQcdc5BjDCJEGST9T3AZvZ+bXUYhMhtzFlWmZvEDKAM9L8CDZ0EAAAAASUVORK5CYII="
-    test_photo_path, test_photo_uuid = InsureeConfig.insuree_photos_root_path, str(uuid.uuid4())
+    photo_base64 = None
+    test_photo_path, test_photo_uuid = None, None
 
     class BaseTestContext:
         def __init__(self, user):
             self.user = user
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_photo_path=InsureeConfig.insuree_photos_root_path
+        cls.test_photo_uuid = str(uuid.uuid4())
+        cls.photo_base64 = "iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAOxAAADsQBlSsOGwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAB7SURBVAiZLc0xDsIwEETRP+t1wpGoaDg6DUgpEAUNNyH2DkXon/S03W+uKiIaANmS07Jim2ytc75cAWMbAJF8Xg9iycQV1AywALCh9yTWtXN4Yx9Agu++EyAkA0IxQQcdc5BjDCJEGST9T3AZvZ+bXUYhMhtzFlWmZvEDKAM9L8CDZ0EAAAAASUVORK5CYII="
 
-    def setUp(self):
-        super(InsureePhotoTest, self).setUp()
-        self._TEST_USER = self._get_or_create_user_api()
-        self.insuree = create_test_insuree(
+        cls._TEST_USER = self._get_or_create_user_api()
+        cls.insuree = create_test_insuree(
             custom_props={'chf_id': '110707070'})
-        self.row_sec = settings.ROW_SECURITY
+        cls.row_sec = settings.ROW_SECURITY
         settings.ROW_SECURITY = False
 
     def tearDown(self) -> None:
@@ -72,7 +75,7 @@ class InsureePhotoTest(TestCase):
     @mock.patch('insuree.services.InsureeConfig')
     @mock.patch('insuree.services.create_file')
     def test_add_photo_save_files(self, create_file, insuree_config):
-        create_file.return_value = self.test_photo_path, self.test_photo_uuid
+        create_file.return_value = self., self.test_photo_uuid
         insuree_config.insuree_photos_root_path = PropertyMock(
             return_value="insuree_root_path")
         insuree_config.get_insuree_number_validator = PropertyMock(
@@ -95,7 +98,7 @@ class InsureePhotoTest(TestCase):
     @mock.patch('insuree.gql_queries.load_photo_file')
     def test_pull_photo_file_path(self, load_photo_file, create_file, insuree_config, insuree_config2):
         load_photo_file.return_value = self.photo_base64
-        create_file.return_value = self.test_photo_path, self.test_photo_uuid
+        create_file.return_value = self., self.test_photo_uuid
         insuree_config.insuree_photos_root_path = PropertyMock(
             return_value="insuree_root_path")
         insuree_config.get_insuree_number_validator = PropertyMock(
@@ -121,7 +124,7 @@ class InsureePhotoTest(TestCase):
         gql_photo = query_result['data']['insurees']['edges'][0]['node']['photo']
         self.assertEqual(gql_photo['photo'], self.photo_base64)
         load_photo_file.assert_called_once_with(
-            self.test_photo_path, str(self.test_photo_uuid))
+            self., str(self.test_photo_uuid))
 
     def __call_photo_mutation(self):
         mutation = self.__update_photo_mutation(self.insuree, self._TEST_USER)
