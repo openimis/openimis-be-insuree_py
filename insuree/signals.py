97b8e2f22667228bd90c2ce50c41068217430dab
delcroip <patrick.delcroix@swisstph.ch>
2024-02-06 11:46:27 +0100

    django 4.2

--- insuree/signals.py
+++ insuree/signals.py
@@ -2,9 +2,9 @@ from core.signals import Signal
 
 
 _insuree_policy_before_query_signal_params = ["user", "additional_filter"]
-signal_before_insuree_policy_query = Signal(providing_args=_insuree_policy_before_query_signal_params)
-signal_before_family_query = Signal(providing_args=_insuree_policy_before_query_signal_params)
-signal_before_insuree_search_query = Signal(providing_args=_insuree_policy_before_query_signal_params)
+signal_before_insuree_policy_query = Signal(_insuree_policy_before_query_signal_params)
+signal_before_family_query = Signal(_insuree_policy_before_query_signal_params)
+signal_before_insuree_search_query = Signal(_insuree_policy_before_query_signal_params)
 
 
 def _read_signal_results(result_signal):
