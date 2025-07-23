6b0b3c3f87d5a72dd06c00d20849cdea01cfa4d6
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-07-23 09:05:59 +0100

    Update test_graphql.py: remove  cls.test_village.parent.parent.code

--- insuree/tests/test_graphql.py
+++ insuree/tests/test_graphql.py
@@ -58,7 +58,7 @@ class InsureeGQLTestCase(openIMISGraphQLTestCase):
         cls.ca_user = create_test_interactive_user(username="testLocationNoRight", roles=[9])
         cls.ca_token = get_token(cls.ca_user, DummyContext(user=cls.ca_user))
         cls.admin_dist_user = create_test_interactive_user(username="testLocationDist")
-        assign_user_districts(cls.admin_dist_user, ["R1D1", "R2D1", "R2D2", "R2D1", cls.test_village.parent.parent.code])
+        assign_user_districts(cls.admin_dist_user, ["R1D1", "R2D1", "R2D2", "R2D1"])
         cls.admin_dist_token = get_token(cls.admin_dist_user, DummyContext(user=cls.admin_dist_user))
         cls.photo_base64 = "iVBORw0KGgoAAAANSUhEUgAAAQAAAAEAAQMAAABmvDolAAAAA1BMVEW10NBjBBbqAAAAH0lEQVRoge3BAQ0AAADCoPdPbQ43oAAAAAAAAAAAvg0hAAABmmDh1QAAAABJRU5ErkJggg=="
         cls.eo_user = create_test_interactive_user(username="Positif")
