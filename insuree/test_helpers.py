c12048969f1714ab5324ac2fdc227aac962a5e57
Piotr Sroka <5571991+piotrsroka@users.noreply.github.com>
2026-08-21 14:38:39 +0200

    refactor(insuree): build insuree test helpers from factories

--- insuree/test_helpers.py
+++ insuree/test_helpers.py
@@ -1,6 +1,7 @@
 from insuree.apps import InsureeConfig
 from insuree.models import Insuree, Family, Gender, InsureePhoto, Profession, Education, Relation, ConfirmationType, FamilyType, IdentificationType
 from insuree.services import validate_insuree_number
+from insuree.test_factories import FamilyFactory, InsureeFactory, InsureePhotoFactory
 from location.models import Location
 from location.test_helpers import create_test_village
 
@@ -76,18 +77,11 @@ def create_test_insuree(with_family=True, is_head=False, custom_props=None, fami
         
 
         
-        insuree = Insuree.objects.create(
+        insuree = InsureeFactory(
             **{
-                'last_name': get_from_custom_props(custom_props, 'last_name',"Test Last" ),
-                'other_names': get_from_custom_props(custom_props, 'other_names', "First Second"),
                 'family': family,
-                'gender': get_from_custom_props(custom_props, 'gender', Gender.objects.get(code='M')),
-                'dob': get_from_custom_props(custom_props, 'dob', '1972-08-09'),
                 'chf_id': ref,
                 'head': is_head,
-                'card_issued': get_from_custom_props(custom_props, 'card_issued', True),
-                'validity_from': get_from_custom_props(custom_props, 'validity_from',"2019-01-01"),
-                'audit_user_id': get_from_custom_props(custom_props, 'audit_user_id',-1),
                 'current_village': village,
                 **custom_props
             }
@@ -140,10 +134,8 @@ def create_test_family(custom_props=None):
         head_insuree = custom_props.pop('head_insuree', Insuree.objects.filter(validity_to__isnull=True).first())    
             
 
-        family = Family.objects.create(
+        family = FamilyFactory(
             **{
-                'validity_from': get_from_custom_props(custom_props, 'validity_from', "2019-01-01"),
-                'audit_user_id': get_from_custom_props(custom_props, 'audit_user_id',-1),
                 'head_insuree': head_insuree,
                 'location': location,
                 **custom_props
@@ -154,16 +146,6 @@ def create_test_family(custom_props=None):
 
 
 
-base64_blank_jpg = """
-/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL
-/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAABAAEDASIAAhEBAxEB/8
-QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2Jyg
-gkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLD
-xMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ
-3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eH
-l6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD3+
-iiigD//2Q==
-"""
 def get_from_custom_props( custom_props, elm, default):
 
     value= custom_props.pop(elm) if custom_props and elm in custom_props else default
@@ -182,16 +164,10 @@ def create_test_photo(insuree_id, officer_id, custom_props=None):
         custom_props = {}
     else:
         custom_props = {k: v for k, v in custom_props.items() if hasattr(InsureePhoto, k)}
-    photo = InsureePhoto.objects.create(
+    photo = InsureePhotoFactory(
         **{
             "insuree_id": insuree_id,
-            "folder": InsureeConfig.insuree_photos_root_path,
             "officer_id": officer_id,
-            "chf_id": "chfpic",
-            "date": "2020-01-01",
-            "validity_from": "2019-01-01",
-            "audit_user_id": -1,
-            "photo": base64_blank_jpg,
             **custom_props
         }
     )
